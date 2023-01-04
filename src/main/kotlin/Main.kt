f992ae8d96d55d134aa8365fd57fc3d95a6605c1
BorlandDev <minsk1115596@gmail.com>
2023-01-04 21:59:17 +0200

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+const val HERO_NAME = "Madrigal"
+
+fun main() {
+    println("The hero announces her presence to the world.")
+
+
+    var playerLevel = 4
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+}
